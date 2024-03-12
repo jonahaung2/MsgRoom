283cf2ec8ee63787342e26e46766dfb7c5bd378a
Aung Ko Min <jonahaung@gmail.com>
2024-03-12 16:49:28 +0800

    Initial Commit

---
+++ MsgRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MsgRoom
+//
+//  Created by Aung Ko Min on 12/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MsgRoom/MsgRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MsgRoomApp.swift
+//  MsgRoom
+//
+//  Created by Aung Ko Min on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MsgRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
